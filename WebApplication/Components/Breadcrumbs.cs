456612e5cc2e70c4f3beedf41ee73d9fd30aa1dc
Vladimir_Dolidze <Vladimir_Dolidze@epam.com>
2021-11-09 22:02:08 +0400

    Added a component

---
+++ WebApplication/Components/Breadcrumbs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Components
+{
+    public class Breadcrumbs : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var path = base.Request.Path;
+            return Content("Test");
+        }
+    }
+}
