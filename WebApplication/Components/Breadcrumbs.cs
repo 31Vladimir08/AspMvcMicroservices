5fc7cc8572f6ee6678d89730c737b76c9e36f660
Vladimir_Dolidze <Vladimir_Dolidze@epam.com>
2021-11-10 22:41:42 +0400

    Added breadcrumbs

--- WebApplication/Components/Breadcrumbs.cs
+++ WebApplication/Components/Breadcrumbs.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+using WebApplication.ViewModels;
 
 namespace WebApplication.Components
 {
@@ -10,8 +11,36 @@ namespace WebApplication.Components
     {
         public IViewComponentResult Invoke()
         {
-            var path = base.Request.Path;
-            return Content("Test");
+            var res = new SitemapElementViewModel();
+            res.SitemapElements = GetBreadcrumbs();
+            return View(res);
+        }
+
+        private IEnumerable<SitemapElement> GetBreadcrumbs()
+        {
+            var i = 0;
+            var result = XElement.Load("sitemap.xml")
+                .Descendants("mvcSiteMapNode")
+                .FirstOrDefault(x =>
+                    x.Attribute("action")?.Value == Request.RouteValues["action"]?.ToString()
+                    &&
+                    x.Attribute("controller")?.Value == Request.RouteValues["controller"]?.ToString())?
+                .AncestorsAndSelf()
+                .Select(
+                    x =>
+                    {
+                        var t = x.Attributes().ToList();
+                        return new SitemapElement()
+                        {
+                            ActionName = t.FirstOrDefault(x => x.Name == "action").Value,
+                            ControllerName = t.FirstOrDefault(x => x.Name == "controller").Value,
+                            Title = t.FirstOrDefault(x => x.Name == "title").Value,
+                            ParentLevel = i++
+                        };
+                    })
+                .OrderByDescending(x => x.ParentLevel)
+                .ToList();
+            return result;
         }
     }
 }
