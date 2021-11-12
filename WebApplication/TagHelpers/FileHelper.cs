c77edc85f5bbcda284dbd49f938e6b1848b58ca6
Vladimir_Dolidze <Vladimir_Dolidze@epam.com>
2021-11-12 09:42:38 +0400

    Added Helpers

---
+++ WebApplication/TagHelpers/FileHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApplication.TagHelpers
+{
+    public static class FileHelper
+    {
+        public static HtmlString NorthwindImageLink(this IHtmlHelper html, int imageID, string linkText)
+        {
+            var s = $@"<a href=""/images/{imageID}"">{linkText}</a>";
+            return new HtmlString(s);
+        }
+    }
+}
