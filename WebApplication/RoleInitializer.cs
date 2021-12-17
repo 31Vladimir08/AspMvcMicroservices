de63a627a9c8d69db35003bc15a7fce9789c7d18
Vladimir_Dolidze <Vladimir_Dolidze@epam.com>
2021-12-17 23:19:03 +0400

    1

--- WebApplication/RoleInitializer.cs
+++ WebApplication/RoleInitializer.cs
@@ -9,22 +9,21 @@ namespace WebApplication
         {
             var adminEmail = "admin@admin.com";
             var password = "$Admin12345$";
-            
-            if (userManager.FindByEmailAsync("admin@admin.com").Result == null)
+
+            if (userManager.FindByEmailAsync("admin@admin.com").Result != null) 
+                return;
+            var user = new IdentityUser
             {
-                var user = new IdentityUser
-                {
-                    Email = adminEmail,
-                    UserName = adminEmail,
-                    NormalizedUserName = "Admin"
-                };
+                Email = adminEmail,
+                UserName = adminEmail,
+                NormalizedUserName = "Admin"
+            };
 
-                var result = userManager.CreateAsync(user, password).Result;
+            var result = userManager.CreateAsync(user, password).Result;
 
-                if (result.Succeeded)
-                {
-                    userManager.AddToRoleAsync(user, nameof(RoleEnum.Administrator)).Wait();
-                }
+            if (result.Succeeded)
+            {
+                userManager.AddToRoleAsync(user, nameof(RoleEnum.Administrator)).Wait();
             }
         }
     }
