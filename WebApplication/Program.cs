4ff7c7d22dbc9dbf4b00c4226fd1eeb8c583b3c6
Vladimir <vladimir86udm@gmail.com>
2023-01-14 15:06:35 +0400

    temp

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Reflection;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace WebApplication
 {
@@ -14,6 +19,24 @@ namespace WebApplication
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(
+                        new ElasticsearchSinkOptions(
+                            new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+                        {
+                            IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".","-")}-logs-{DateTime.UtcNow:yyyy-MM}",
+                            AutoRegisterTemplate= true,
+                            NumberOfReplicas = 1,
+                            NumberOfShards = 2,
+                        })
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .ReadFrom.Configuration(context.Configuration);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureAppConfiguration(
@@ -22,12 +45,6 @@ namespace WebApplication
                             config.AddJsonFile("appsettings.json", true);
                         })
                     .UseStartup<Startup>();
-                })
-                .ConfigureLogging((context, builder) 
-                    =>
-                    {
-                        builder.AddConfiguration(context.Configuration.GetSection("Logging"));
-                        builder.AddFile();
-                    });
+                });
     }
 }
