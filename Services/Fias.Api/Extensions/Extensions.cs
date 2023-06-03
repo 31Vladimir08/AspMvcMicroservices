20080a0c9e38cd81f2ae5348acc8e925e1b1d445
Vladimir <vladimir86udm@gmail.com>
2023-06-03 10:36:03 +0400

    1

--- Services/Fias.Api/Extensions/Extensions.cs
+++ Services/Fias.Api/Extensions/Extensions.cs
@@ -11,60 +11,60 @@ namespace Fias.Api.Extensions
                 : property.PropertyType;
             if (string.IsNullOrWhiteSpace(value))
             {
-                if (property.PropertyType.Name != "Nullable`1")
-                    return;
-                else
+                if (property.PropertyType.Name == "Nullable`1" || property.PropertyType.Name == "String")
                 {
                     property.SetValue(obj, null);
                     return;
                 }
-            }
-
-            if (propertyType is not null)
-            {
-                switch (propertyType.Name)
+                else
                 {
-                    case "UInt16":
-                    case "UInt32":
-                    case "UInt64":
-                    case "UInt128":
-                    case "UIntPtr":
-                        property.SetValue(obj, uint.Parse(value));
-                        break;
-                    case "Int16":
-                    case "Int32":
-                    case "Int64":
-                    case "Int128":
-                        property.SetValue(obj, int.Parse(value));
-                        break;
-                    case "Single":
-                        property.SetValue(obj, float.Parse(value));
-                        break;
-                    case "Double":
-                        property.SetValue(obj, double.Parse(value));
-                        break;
-                    case "Decimal":
-                        property.SetValue(obj, decimal.Parse(value));
-                        break;
-                    case "Byte":
-                        property.SetValue(obj, byte.Parse(value));
-                        break;
-                    case "DateTime":
-                        property.SetValue(obj, DateTime.Parse(value));
-                        break;
-                    case "Boolean":
-                        property.SetValue(obj, bool.Parse(value));
-                        break;
-                    case "String":
-                    default:
-                        property.SetValue(obj, value);
-                        break;
+                    return;
                 }
-
             }
-            else
-            {
 
+            if (propertyType is null)
+                return;
+
+            switch (propertyType.Name)
+            {
+                case "UInt16":
+                case "UInt32":
+                case "UInt64":
+                case "UInt128":
+                case "UIntPtr":
+                    property.SetValue(obj, uint.Parse(value));
+                    break;
+                case "Int16":
+                case "Int32":
+                case "Int64":
+                case "Int128":
+                    property.SetValue(obj, int.Parse(value));
+                    break;
+                case "Single":
+                    property.SetValue(obj, float.Parse(value));
+                    break;
+                case "Double":
+                    property.SetValue(obj, double.Parse(value));
+                    break;
+                case "Decimal":
+                    property.SetValue(obj, decimal.Parse(value));
+                    break;
+                case "Byte":
+                    property.SetValue(obj, byte.Parse(value));
+                    break;
+                case "DateTime":
+                    property.SetValue(obj, DateTime.Parse(value));
+                    break;
+                case "Guid":
+                    property.SetValue(obj, Guid.Parse(value));
+                    break;
+                case "Boolean":
+                    property.SetValue(obj, bool.Parse(value));
+                    break;
+                case "String":
+                default:
+                    property.SetValue(obj, value);
+                    break;
             }
         }
     }
